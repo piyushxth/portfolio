8a087d485ad78955e4fa5b8d5b5475a503197326
piyushxth <piyush.xtha7@gmail.com>
2025-07-23 20:55:10 +0545

    bar in experience component fixed

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -170,7 +170,7 @@ const Experience = () => {
       <div className="relative divide-y divide-gray-100">
         {/* Timeline vertical line & image */}
         <div className="absolute top-0 left-[5%] md:left-[25%] h-full w-12 ">
-          <div className="absolute top-0 bottom-0 left-1/2 w-2 -translate-x-1/2 rounded-full bg-[#E8ECEF] shadow-[inset_0_2px_1.5px_rgba(165,174,184,0.32)]" />
+          <div className="absolute top-0 bottom-0 left-1/2 w-1 -translate-x-1/2 rounded-full bg-[#E8ECEF] shadow-[inset_0_2px_1.5px_rgba(165,174,184,0.32)]" />
 
           <div className="sticky top-1/2 -translate-y-1/2">
             <div className="relative mx-auto h-10 w-10 overflow-hidden rounded-full border-2 border-primary bg-primary shadow-md">
